d645aae1b4a11638999a2f89b0bcbc75a40a1df1
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2023-01-09 16:35:04 +0100

    Update webflow-script.js

--- webflow-script.js
+++ webflow-script.js
@@ -1,26 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.querySelector("#pwnd-form");
 
-    form.addEventListener("submit", () => {
-        const input = form.querySelector("#pwnd-mail");
-        const msg = form.querySelector("#pwnd-msg");
-
-        fetch("api-url", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ email: input.value }),
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                const info = data.message;
-                msg.style.display = "flex";
-                msg.innerHTML  = info;
+    Webflow.push(function() {
+        $("form").submit(function() {
+            const input = form.querySelector("#pwnd-mail");
+            const msg = form.querySelector("#pwnd-msg");
+    
+            fetch("api-url", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ email: input.value }),
             })
-            .catch((e) => {
-                msg.style.display = "flex";
-                msg.innerHTML  = "Something went wrong. Try again later.";
-            });
+                .then((res) => res.json())
+                .then((data) => {
+                    const info = data.message;
+                    msg.style.display = "flex";
+                    msg.innerHTML  = info;
+                })
+                .catch((e) => {
+                    msg.style.display = "flex";
+                    msg.innerHTML  = "Something went wrong. Try again later.";
+                });
+        });
     });
 });
