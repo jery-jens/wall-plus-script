a704dbbfca86f3bc0d35bd4b4be9f3e8b55b0db7
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2023-01-09 15:43:52 +0100

    first commit

---
+++ webflow-script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("#pwnd-form");
+
+    form.addEventListener("submit", () => {
+        const input = form.querySelector("#pwnd-mail");
+        const msg = form.querySelector("#pwnd-msg");
+
+        fetch("api-url", {
+            method: "POST",
+            body: JSON.stringify({ email: input.value }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                const info = data.message;
+                msg.style.display = "flex";
+                msg.innerHTML  = info;
+            })
+            .catch((e) => {
+                msg.style.display = "flex";
+                msg.innerHTML  = "Something went wrong. Try again later.";
+            });
+    });
+});
